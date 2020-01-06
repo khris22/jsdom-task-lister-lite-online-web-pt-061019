6a92c7f71b7a03935931401102b2030182c55249
Khris Punzalan <khrispunzalan@gmail.com>
2020-01-06 05:51:34 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  // set variable for selected HTML elements from the DOM
   const taskForm = document.getElementById("create-task-form")
   const inputField = document.getElementById("new-task-description");
   // const submitButton = document.getElementById("submit");
   const listTasks = document.getElementById("tasks");
 
-
   taskForm.addEventListener("submit", function(event) {
       event.preventDefault();
       const newTask = document.createElement("li");
-      newTask.innerHTML = document.getElementById("new-task-description").value;
+      newTask.innerHTML = inputField.value;
       listTasks.appendChild(newTask);
       event.target.reset();
     }, false);
-
 });
+
+
+// event.target.reset();
+// target event property returns the element that triggered the event.
+// reset() method resets the values of all elements in a form
+
+// Creating an element in JavaScript is an easy process. Simply call document.createElement('tagName'), where tagName is the name of any valid HTML tag (e.g., 'p', 'div', 'span', etc.).
+// You can update that node's innerHTML property with a string of HTML and it will be just as if you changed the HTML source for that node.
+// appendChild() ; to get an element to appear in the DOM, we have to append it to an existing DOM node.
